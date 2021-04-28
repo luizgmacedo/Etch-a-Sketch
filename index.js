f4ba694d3726606260bbcd44ceb47d1ce93cb934
Luiz Gustavo <gu_macedo07@hotmail.com>
2021-04-28 18:07:59 -0300

    update

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ function createGrid(size) {
             const square = document.createElement('div');
             square.classList.add('square');
             grid.appendChild(square);
-            hoverEffect();
+            hoverEffect('black');
         }
     }
     grid.style['grid-template-columns'] = `repeat(${size}, 1fr)`;
 }
 
-function hoverEffect() {
+function hoverEffect(color) {
     const squares = document.querySelectorAll('.square');
 
     squares.forEach(square => {
         square.addEventListener('mouseover', e => {
-            e.target.style.backgroundColor = 'black';
+            e.target.style.backgroundColor = color;
         });
     });
 }
@@ -30,8 +30,48 @@ function hoverEffect() {
 function clearGrid() {
     grid.innerHTML = '';
     let size = parseInt(prompt('Grid size: '));
+    if (size <= 0 || !size) size = 16;
+    while (size > 64) {
+        alert('Grid size must be lower than 64');
+        size = parseInt(prompt('Grid size: '));
+    }
     createGrid(size);
 }
+
+function randomColor() {
+    let color = '#';
+    let digits = 'ABCDEF0123456789';
+
+    for (let i = 0; i < 6; i++) {
+        color += digits[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function grayScale(color) {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        let opacity = 0.2;
+        square.addEventListener('mouseover', e => {
+            e.target.style.backgroundColor = 'black';
+            e.target.style.opacity = opacity;
+            opacity += 0.1;
+        });
+    });
+}
+
+colorBtn.addEventListener('click', e => {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.addEventListener('mouseover', e => {
+            let color = randomColor();
+            e.target.style.backgroundColor = color;
+            e.target.style.opacity = 1;
+        });
+    });
+});
+
+grayScaleBtn.addEventListener('click', grayScale);
 
 clearBtn.addEventListener('click', clearGrid);
 
